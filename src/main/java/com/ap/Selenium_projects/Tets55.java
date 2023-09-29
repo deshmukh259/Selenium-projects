524ae2fb0309846cec90487762b245bf2d81865d
purushottamdeshmukh <deshmukh259.2010@gmail.com>
2023-09-29 13:43:39 +0530

    c

--- src/main/java/com/ap/Selenium_projects/Tets55.java
+++ src/main/java/com/ap/Selenium_projects/Tets55.java
@@ -1,4 +1,14 @@
 package com.ap.Selenium_projects;
 
 public class Tets55 {
+    public static void main(String[] args) {
+        String cc = "diaplyyyyy";
+        AA s =(e,v)-> {
+            System.out.println(e+v);
+        };
+        s.display(cc,cc);
+    }
+}
+interface AA{
+    void display(String vv,String v);
 }
